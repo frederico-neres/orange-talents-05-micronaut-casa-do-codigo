0a9f438a615ac1b18ed697d78a8e03e0d6c57bac
Frederico Neres <neresfrederico@gmail.com>
2021-07-01 11:48:31 -0300

    Adicionado validações no DTO de novo autor

--- src/main/kotlin/br/com/zup/autores/CadastrarAutorController.kt
+++ src/main/kotlin/br/com/zup/autores/CadastrarAutorController.kt
@@ -1,14 +1,16 @@
 package br.com.zup.autores
 
-import io.micronaut.http.annotation.Body
-import io.micronaut.http.annotation.Controller
-import io.micronaut.http.annotation.Post
+import io.micronaut.http.MediaType
+import io.micronaut.http.annotation.*
+import io.micronaut.validation.Validated
+import javax.validation.Valid
 
+@Validated
 @Controller("/api/autores")
 class CadastrarAutorController {
 
     @Post
-    fun cadastrar(@Body request: NovoAutorRequest){
+    fun cadastrar(@Body @Valid request: NovoAutorRequest){
         println(request)
     }
 }
