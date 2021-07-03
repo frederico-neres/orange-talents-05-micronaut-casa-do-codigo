79bdb4037a7fb065bc3f704d277f0bfd64829e0f
Frederico Neres <neresfrederico@gmail.com>
2021-07-03 15:51:46 -0300

    Recebendo dados via DELETE

---
+++ src/main/kotlin/br/com/zup/autores/DeletarAutorController.kt
@@ -0,0 +1,20 @@
+package br.com.zup.autores
+
+import io.micronaut.http.HttpResponse
+import io.micronaut.http.annotation.Controller
+import io.micronaut.http.annotation.Delete
+import io.micronaut.http.annotation.PathVariable
+
+@Controller("/api/autores/{id}")
+class DeletarAutorController(private val autorRepository: AutorRepository) {
+
+    @Delete
+    fun deletar(@PathVariable id: Long): HttpResponse<Any> {
+        val existsById = autorRepository.existsById(id)
+        if(!existsById) return HttpResponse.badRequest()
+
+        autorRepository.deleteById(id)
+        return HttpResponse.ok()
+    }
+
+}
